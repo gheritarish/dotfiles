f160b59e894e0f22d6a0a07e43587304e0286548
baptiste-marlet <baptiste.marlet@paylead.fr>
2023-06-16 11:44:13 +0200

    chore(nvim): Disable treesitter for more extensions

--- nvim/init.lua
+++ nvim/init.lua
@@ -91,7 +91,7 @@ require'nvim-treesitter.configs'.setup {
     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
     -- the name of the parser)
     -- list of language that will be disabled
-    disable = { "c", "rust", "markdown", "sql", "gitcommit" },
+    disable = { "c", "rust", "markdown", "sql", "gitcommit", "htmldjango", "git", "diff" },
     -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
     --disable = function(lang, buf)
         --local max_filesize = 100 * 1024 -- 100 KB
