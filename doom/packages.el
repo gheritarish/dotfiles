6abc835b960859f5fd2b0770fb1bf8f7d534bed1
baptiste-marlet <baptiste.marlet@padam.io>
2025-03-07 18:29:42 +0100

    chore(emacs): Improve emacs config for work

--- doom/packages.el
+++ doom/packages.el
@@ -7,9 +7,15 @@
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 (package! catppuccin-theme)
+(package! reformatter)
 (package! flymake)
 (package! lsp-mode)
 (package! eglot)
+(package! ruff-format)
+(package! pyvenv)
+(package! tree-sitter-langs)
+(package! elfeed)
+(package! elfeed-org)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
